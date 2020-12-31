6f43e1fd2316500613f195cfcee97932e2db05fa
Sam Carne <sam@samcarne.com>
2020-12-30 19:29:54 -0500

    blank configuration file for use

---
+++ config-blank.php
@@ -0,0 +1,25 @@
+<?php
+
+//enter hosting info
+define('HOST', '');
+define('USERNAME', '');
+define('PASSWORD', '');
+define('DBNAME', '');
+
+
+// I used a table for each round that i can alter here
+// to apply throughout the site
+define('TABLENAME', '');
+
+
+// I use this for a menu if you have one, otherwise you
+// don't have to use this default. This is just something
+// I commonly have in my config files.
+define('BASEURL'. '');
+
+
+// In my tables, I use a primary key, a grade, question,
+// answer, score, and points. feel free to alter for your
+// liking
+
+?>
